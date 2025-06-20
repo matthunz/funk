c10e45113563d7a61eade72061846d365f84908b
Matt Hunzinger <matt@hunzinger.me>
2025-06-19 21:00:44 -0400

    Setup tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,24 @@
-module Main (main) where
+module Main where
+
+import Data.Either
+import Funk (showErrorPretty, tryRun)
+import Funk.SExpr (sExprPretty)
+import Funk.Solver (sTermToSExpr)
+import Test.Hspec
+
+run :: String -> IO ()
+run input = do
+  res <- tryRun input
+  res' <- case res of
+    Right t -> do
+      expr <- sTermToSExpr t
+      return . Right $ sExprPretty expr
+    Left e -> do
+      eStr <- showErrorPretty e input
+      return $ Left eStr
+  res' `shouldSatisfy` isRight
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec $ do
+  describe "Prelude.head" $ do
+    it "compiles true" $ run "(forall (a) (fn (x a) (fn (y a) x)))"
